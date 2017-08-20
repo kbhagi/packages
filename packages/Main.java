2537f0074cffe11cd1730588c103b8b5f34645d7
bhargavak <bhargavak37@gmail.com>
2017-08-20 14:07:44 +0530

    Place classes in their package in Core Java
    Place classes in their package in Core Java

---
+++ packages/Main.java
@@ -0,0 +1,10 @@
+package com.bhargavak;
+
+import javafx.scene.Node;
+
+public class Main {
+	public static void main(String[] args) {
+		Node node = null;
+		//javafx.scene.Node node = null;
+	}
+}
---
+++ packages/src/Main.java
@@ -0,0 +1,9 @@
+package com.bhargavak;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		MyWindow MyWindow = new MyWindow("Complete Java");
+		MyWindow.setVisible(true);
+	}
+}
